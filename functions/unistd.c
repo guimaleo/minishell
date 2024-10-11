f96ccff57d4e04284f2df398d4760dc999941e28
guimaleo <leodeguimaraes@gmail.com>
2024-10-11 16:05:07 +0100

    Home update: 16:05:00 11-10-2024
    Added the output images

--- functions/unistd.c
+++ functions/unistd.c
@@ -3,15 +3,17 @@
 
 // int access(const char *path, int amode)
 
-int		main(void)
+int	main()
 {
-	int		res[3];
-	int		i;
+	pid_t	pid;
 
-	res[0] = access("/bin/ls", 0);	//Returns 0, as right execution
-	res[1] = access("ls", 0);		//Without the absolute path returns an error
-	res[2] = access("/bin/cd", 0);	//Since the "cd" command doesn't exist in the /bin, returns an error
-	for(i = 0; i < 3; i++)
-		printf("Access return: %i\n", res[i]);
+	printf("Fork wait for child call\n\n");
+	pid = fork();
+	if (pid != 0) // If it's not the child
+	{
+		wait(NULL);
+		printf("I've been waiting my child!\n");
+	}
+	else
+		printf("Parent process will wait for me!\n");
 }
-
