2b9f3fab5f259acc1caed81629c3e60733138861
guimaleo <leodeguimaraes@gmail.com>
2024-10-26 11:40:30 +0100

    Home update: 11:40:16 26-10-2024
    Added descriptions to the lexical functions

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -3,73 +3,89 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lede-gui <lede-gui@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lede-gui <lede-gui@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 22:44:54 by lede-gui          #+#    #+#             */
-/*   Updated: 2024/10/25 23:27:55 by lede-gui         ###   ########.fr       */
+/*   Updated: 2024/10/26 11:39:07 by lede-gui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
-static int    redirections(char *str, char **input, size_t *i)
+/**
+ * @brief This function will recognize any of the redirections tokens,
+ * and inject the right escape sequence to it, to be splitted
+ * @param str -> the new string with the injections
+ * @param input -> the input from the prompt
+ * @param i -> the position to be injected
+ */
+static int	redirections(char *str, char **input, size_t *i)
 {
-    if ((input[0][0] == '<' && input[0][1] == '<') || ((input[0][0] == '>' \
-    && input[0][1] == '>')))
-    {
-        str[(*i)++] = '2';
-        str[(*i)++] =  (**input);
-        str[(*i)++] =  (**input);
-        str[(*i)++] = '2'; 
-        (*input) += 2;
-        return (1);
-    }
-    else if ((input[0][0] == '>') || (input[0][0] == '<'))
-    {
-        str[(*i)++] = '2';
-        str[(*i)++] =  (**input);
-        str[(*i)++] = '2'; 
-        (*input)++;
-        return (1);
-    }
-    else if (input[0][0] == '|')
-    {
-        str[(*i)++] =  '3';
-        (*input)++;
-        return (1);
-    }
-    return (0);
+	if ((input[0][0] == '<' && input[0][1] == '<') || ((input[0][0] == '>' \
+	&& input[0][1] == '>')))
+	{
+		str[(*i)++] = '2';
+		str[(*i)++] =  (**input);
+		str[(*i)++] =  (**input);
+		str[(*i)++] = '2';
+		(*input) += 2;
+		return (1);
+	}
+	else if ((input[0][0] == '>') || (input[0][0] == '<'))
+	{
+		str[(*i)++] = '2';
+		str[(*i)++] =  (**input);
+		str[(*i)++] = '2';
+		(*input)++;
+		return (1);
+	}
+	else if (input[0][0] == '|')
+	{
+		str[(*i)++] =  '3';
+		(*input)++;
+		return (1);
+	}
+	return (0);
 }
 
-static void    tokenization(char *str, char *input)
+/**
+ *  @brief This function takes a flag to recognize if the string of input is in single
+ * or double quotes, and if it its, it should not make any change in the quoted string
+ * @param input -> fetched from readline
+ * @param str -> the new string 3x bigger than the original input
+ */
+static void	tokenization(char *str, char *input)
 {
-    char    flag;
-    size_t     i;
+	char	flag;
+	size_t	 i;
 
-    flag = 0;
-    i = 0;
-    while(*input)
-    {
-        if (flag == 0 && (*input == '\"' || *input == '\''))
-            flag = *input;
-        else if (flag == *input)
-            flag = 0;
-        if (flag == 0 && *input == ' ')
-            *input = '2';
-        if (!(flag == 0 && redirections(str, &input, &i)))
-            str[i++] = *input++;
-    }
+	flag = 0;
+	i = 0;
+	while(*input)
+	{
+		if (flag == 0 && (*input == '\"' || *input == '\''))
+			flag = *input;
+		else if (flag == *input)
+			flag = 0;
+		if (flag == 0 && *input == ' ')
+			*input = '2';
+		if (!(flag == 0 && redirections(str, &input, &i)))
+			str[i++] = *input++;
+	}
 }
-
-void    lexer(char *input)
+/**
+ * @brief Takes the input and allocates a string 3x bigger to inject
+ * substitution values for the split of the tokens
+ * @param input -> fetched from the readline
+ */
+void	lexer(char *input)
 {
-    char    *str;
+	char	*str;
 
-    str = ft_calloc(ft_strlen(input), 3);
-    if (!str)
-        return ;
-    tokenization(str, input);
-    printf("str: %s\n", str);
-    free(str);
+	str = ft_calloc(ft_strlen(input), 3);
+	if (!str)
+		return ;
+	tokenization(str, input);
+	printf("str: %s\n", str);
+	free(str);
 }
